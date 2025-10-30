7bc0a5911df6b9a31f9b53fb0d01cd24b97cd60a
FatemehOstvar <fatemehostvar2005@gmail.com>
2025-10-30 09:56:43 +0000

    headers sit nicely now

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/WeatherApp/", // <-- use '/' only if deploying to USERNAME.github.io root
+  base: "/WeatherApp/",
   build: { outDir: "docs" },
 });
