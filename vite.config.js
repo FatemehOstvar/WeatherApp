3b6397ffbae811f49cf69a22c1b11f2358a63893
FatemehOstvar <fatemehostvar2005@gmail.com>
2026-01-05 22:21:17 +0330

    readied for prod

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, "index.html"), // Your existing entry
-  //       login: resolve(__dirname, "pages/login.html"),
-  //       signup: resolve(__dirname, "pages/signup.html"),
-  //     },
-  //   },
-  // },
-  // server: {
-  // proxy: {
-  //   // "/api": {
-  //   //   target: "http://localhost:3006",
-  //   //   changeOrigin: true,
-  //   //   secure: false,
-  //   // },
-  // },
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "pages/login.html"),
+        signup: resolve(__dirname, "pages/signup.html"),
+        // settings: resolve(__dirname, "pages/settings.html"),
+        cities: resolve(__dirname, "pages/cities.html"),
+      },
+    },
+  },
 });
